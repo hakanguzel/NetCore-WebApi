3a4f23aeeb4ceb8b8e0f2f62750362e42c15f37f
Hakan GÜZEL <hakan-guzel@outlook.com>
2020-08-03 15:36:29 +0300

    Initial commit

---
+++ BasicApi.Business/Concrete/SaleService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BasicApi.Business.Abstract;
+
+namespace BasicApi.Business.Concrete
+{
+    public class SaleService : ISaleService
+    {
+    }
+}
